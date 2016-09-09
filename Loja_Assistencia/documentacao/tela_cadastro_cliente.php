121722fc978c428cebefa3089bd4ddd4496cf352
TADS <TADS@LAB03-EST03>
2016-09-08 21:20:10 -0400

    tela cadastro do cliente

---
+++ Loja_Assistencia/documentacao/tela_cadastro_cliente.php
@@ -0,0 +1,50 @@
+<?php include 'cabecalho.php';       
+?>
+       <div class="container">
+    <div class="Principal">
+        
+        <title> Cadastrar Cliente </title>
+        
+        <h1>Cadastrar Cliente</h1>
+    
+     
+        <form method="post" action="xxxxxxxxx">
+        
+            <table class="table-condensed" width="80%" align="center">
+                <tr>
+                    <td>Nome:</td>
+                    <td colspan="3"><input class="form-control" type="text" name="nome" size="60"/></td>       
+                  
+                </tr>
+                <tr class="form-inline form-group">
+                    <td>RG:</td>
+                    <td> <input class="form-control" type="text"  name="rg" /></td>
+                     <td>CPF:</td>
+                     <td> <input class="form-control" type="text" name="cpf" maxlength="11"/></td> 
+                </tr>                                             
+                <tr>
+                                      
+                </tr>
+                <tr>
+                    <td>Telefone:</td>
+                    <td> <input class="form-control" type="text" name="telefone"/></td>                    
+                </tr>
+                <tr>
+                    <td>Endereço:</td>
+                    <td> <input class="form-control" type="text" name="endereço"/></td>                    
+                </tr>
+                <tr>
+                    <td>Data de Cadastro:</td>
+                    <td> <input class="form-control" type="date" name="nascimento"/></td>                    
+                </tr>  
+                <tr>
+                    <td> <input class="btn btn-primary" type="submit" value="Cadastrar"/></td>
+                </tr>
+                
+            </table>    
+    </form>
+        
+        </div>
+    </div>
+    </body>
+</html>
