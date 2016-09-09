ed28c2741e17bcdead7e46b257666ae02177f925
TADS <TADS@10.1.2.84>
2016-09-08 21:41:39 -0400

    Pesqueisa Cliente

---
+++ Loja_Assistencia/pesquisa_cliente.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>www.consertacel.com</title>
+</head> 
+<body> <center>
+<form name="Form" method="post" id="formulario">
+	
+            <td>Pesquisar Cliente:</td>
+            <input type="text" class="input_text" name="pesquisar" id="pc"/>
+                <input type="button" class="button" value="Pesquisar" />
+                <label><br></br> 
+                    <td>Nome Completo</td>
+                    <input type="text" class="input_text" name="nome" id="name"/>    
+		</label>
+ 
+		<label>
+                    <td>RG</td>
+			<input type="text" class="input_text" name="email" id="rg"/>
+		 </label>
+ 
+		<label>
+ 			<td>CPF</td>
+			<input type="text" class="input_text" name="assunto" id="cpf"/>
+		</label>
+ 
+		<label>
+			 <td>OS.Ativa</td>
+			<input type="text" class="input_text" name="assunto" id="Os.Ativa"/>
+                </label>    
+                
+                
+	
+</form> 
+ </center>
+</body>
+</html>
+
