caf97d1a8102b10ee741ae6c01ece8a1640c4076
TADS <TADS@LAB03-EST03>
2016-09-15 22:17:42 -0400

    Merge origin/master

---
+++ Loja_Assistencia/teste.php
@@ -0,0 +1,75 @@
+<?php include 'cabecalho.php';
+?>
+<div class="container">
+    <div class="Principal">
+        <div class="row">
+            <div class="col-lg-10">
+                <div class="panel panel-default" align="center">
+                    <div class="panel-heading" >
+
+                        <title > Cadastrar Cliente </title>
+                    </div>
+
+                    <h1>Cadastrar Cliente</h1>
+
+                    <form method="post" action="xxxxxxxxx">
+
+                        <div class="table-condensed" width="80%" align="center">
+                            <div class="row" style="margin-left:10px; ">
+                                <div class="form-group col-md-6" style="text-align: left">
+                                    <label for="campo1">Nome</label>
+                                    <input type="text" class="form-control" id="campo1">
+                                </div>
+                                <div class="form-group col-md-3" style="text-align: left">                                    
+                                    <label for="campo1">RG</label>
+                                    <input type="text" class="form-control" id="campo3">
+                                </div>
+                                <div class="form-group col-md-3" style="text-align: left">                                    
+                                    <label for="campo1">CPF</label>
+                                    <input type="text" class="form-control" id="campo3">                                    
+                                </div>
+                            </div>
+
+                            <div class="row" style="margin-left:10px; ">
+                                <div class="form-group col-md-4" style="text-align: left">
+                                    <label for="campo1">Telefone</label>
+                                    <input type="text" class="form-control" id="campo4">
+                                </div>
+                                <div class="form-group col-md-8" style="text-align: left">
+                                    <label for="campo1">Endereço</label>
+                                    <input type="text" class="form-control" id="campo5">
+                                </div>
+                                <div class="row" style="margin-left:10px; ">
+                                    <div class="form-group col-md-6" style="text-align: left">
+                                    <label for="campo1">Bairro</label>
+                                    <input type="text" class="form-control" id="campo6">
+                                </div>
+                                    <div class="form-group col-md-2" style="text-align: left">
+                                    <label for="campo1">Nº</label>
+                                    <input type="text" class="form-control" id="campo6">
+                                </div>
+                                <div class="form-group col-md-4" style="text-align: left">
+                                    <label for="campo1">Data Cadastro</label>
+                                    <input type="text" class="form-control" id="campo6">
+                                </div>
+                                </div>
+                            </div>
+
+                            <tr>
+                                <td colspan="4"><input class="btn btn-primary pull-right"   type="submit" value="Cadastrar"/></td>
+                            </tr>
+
+                            </table> 
+
+
+
+
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+</body>
+</html>
