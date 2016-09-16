6da3862c50892b487ed1a09f6856a6bd947c56d3
TADS <TADS@LAB03-EST03>
2016-09-15 22:14:25 -0400

    tela cadastro do cliente

--- Loja_Assistencia/cadastrar_clientes.php
+++ Loja_Assistencia/cadastrar_clientes.php
@@ -1,50 +1,63 @@
-<?php include 'cabecalho.php';       
+<?php include 'cabecalho.php';
 ?>
-       <div class="container">
-    <div class="Principal">
-        
-        <title> Cadastrar Cliente </title>
-        
-        <h1>Cadastrar Cliente</h1>
-    
-     
-        <form method="post" action="xxxxxxxxx">
-        
-            <table class="table-condensed" align="center">
-                <tr>
-                    <td>Nome:</td>
-                    <td colspan="3"><input class="form-control" type="text" name="nome" size="60"/></td>       
-                  
-                </tr>
-                <tr>
-                    <td>RG:</td>
-                    <td> <input class="form-control" type="text"  name="rg" /></td>
-                     <td>CPF:</td>
-                     <td> <input class="form-control" type="text" name="cpf" /></td> 
-                </tr>                                             
-                <tr>
-                                      
-                </tr>
-                <tr>
-                    <td>Telefone:</td>
-                    <td> <input class="form-control" type="text" name="telefone"/></td>                    
-                </tr>
-                <tr>
-                    <td>Endereço:</td>
-                    <td> <input class="form-control" type="text" name="endereço"/></td>                    
-                </tr>
-                <tr>
-                    <td>Data de Cadastro:</td>
-                    <td> <input class="form-control" type="date" name="nascimento"/></td>                    
-                </tr>  
-                <tr>
-                    <td> <input class="btn btn-primary" type="submit" value="Cadastrar"/></td>
-                </tr>
-                
-            </table>    
-    </form>
-        
+<div class="container">
+<div class="Principal">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default" align="center">
+                <div class="panel-heading" >
+
+                    <title > Cadastrar Cliente </title>
+                </div>
+
+                <h1>Cadastrar Cliente</h1>
+
+
+                <form method="post" action="xxxxxxxxx">
+
+                    <table class="table-condensed" width="80%" align="center">
+                        <tr>
+                            <td>Nome:</td>
+                            <td><input class="form-control" type="text" name="nome" size="60"/></td>       
+
+                        </tr>
+                        <tr>
+                            <td>RG:</td>
+                            <td> <input class="form-control" type="text"  name="rg" /></td>
+                            <td>CPF:</td>
+                            <td> <input class="form-control" type="text" name="cpf" /></td> 
+                        </tr>                                             
+                        <tr>
+
+                        </tr>
+                        <tr>
+                            <td>Telefone:</td>
+                            <td> <input class="form-control" type="text" name="telefone"/></td>                    
+                        </tr>
+                        <tr>
+                            <td>Endereço:</td>
+                            <td> <input class="form-control" type="text" name="endereço"/></td>                    
+                        </tr>
+                        <tr>
+                            <td>Data de Cadastro:</td>
+                            <td> <input class="form-control" type="date" name="nascimento"/></td>                    
+                        </tr>  
+
+                        <tr>
+                            <td colspan="4"><input class="btn btn-primary pull-right"   type="submit" value="Cadastrar"/></td>
+                        </tr>
+
+                    </table> 
+
+
+
+
+                </form>
+            </div>
         </div>
     </div>
-    </body>
+
+</div>
+</div>
+</body>
 </html>
